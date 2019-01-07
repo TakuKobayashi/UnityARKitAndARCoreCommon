df212462be9a3af14aa036478b4b733a02d3383f
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2019-01-07 12:58:39 +0900

    Add ARKit Light

---
+++ Assets/UnityARKitAndARCoreCommon/Scripts/Common/ARDirectionalLight.cs
@@ -0,0 +1,19 @@
+﻿namespace ARKitAndARCoreCommon
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+#if UNITY_IOS
+    using UnityEngine.XR.iOS;
+#endif
+
+    public class ARDirectionalLight : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+#if UNITY_IOS
+            this.gameObject.AddComponent<UnityARAmbient>();
+#endif
+        }
+    }
+}
